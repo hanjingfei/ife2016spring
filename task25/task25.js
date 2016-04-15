1b8fc1db21445726831ce8e1a65303948aa2dbcd
hanjingfei <icsoc@qq.com>
2016-04-15 18:27:53 +0800

    fold is ok

--- task25/task25.js
+++ task25/task25.js
@@ -1,186 +1,156 @@
 var delBoxBtn = document.getElementById("del-box-btn");
 var addBoxBtn = document.getElementById("add-box-btn");
 var newBoxName = document.getElementById("new-box-name");
-var rootBox = document.getElementById("root-box");
 var containerDiv = document.getElementById("container");
+
+var containerBox = null;
+var rootBox = null;
+
+var folded = true;
+var unfold = false;
+var cmdShow = true;
+var cmdHide = false;
+//TreeBox defines the tree node data structure for build/search/render
+function TreeBox(box) {
+  this.parent = box.parent;
+  this.childs = box.childs || [];
+  this.data = box.data || "";
+  this.dom = box.dom;
+  this.dom.TreeBox = this;  //link
+}
+
+TreeBox.prototype = {
+  constructor: TreeBox,
+  render: function(folded, cmdShow) {
+    if(this.childs.length == 0) {
+      this.dom.getElementsByClassName("header")[0].getElementsByClassName("fold-sym")[0].innerHTML = " ";
+    } else {
+      this.dom.getElementsByClassName("header")[0].getElementsByClassName("fold-sym")[0].innerHTML = folded ? "+" : "-";
+      for (var i = 0; i < this.childs.length; i++) {
+        if(this.parent != null) {
+          this.childs[i].dom.className = folded ? "box hide" : "box";
+        }
+      }
+    }
+    if(cmdShow) {
+      this.dom.getElementsByClassName("header")[0].getElementsByClassName("add")[0].className = "add";
+      this.dom.getElementsByClassName("header")[0].getElementsByClassName("del")[0].className = "del";
+    } else {
+      this.dom.getElementsByClassName("header")[0].getElementsByClassName("add")[0].className = "add hide";
+      this.dom.getElementsByClassName("header")[0].getElementsByClassName("del")[0].className = "del hide";
+    }
+  },
+  addChild: function(data) {
+    if(data.trim() == "") {
+      alert("节点内容为空！");
+      return this;
+    }
+    var childBox = document.createElement("div");
+    var childBoxHead = document.createElement("div");
+    var childFoldSym = document.createElement("div");
+    childFoldSym.innerHTML = " ";//+-
+    childFoldSym.className = "fold-sym";
+    childBoxHead.appendChild(childFoldSym);
+    var childData = document.createElement("div");
+    childData.innerHTML = data;
+    childData.className = "data";
+    childBoxHead.appendChild(childData);
+    var childCmdAdd = document.createElement("div");
+    childCmdAdd.innerHTML = "添加";
+    childCmdAdd.className = "add";
+    childBoxHead.appendChild(childCmdAdd);
+    var childCmdDel = document.createElement("div");
+    childCmdDel.innerHTML = "删除";
+    childCmdDel.className = "del";
+    childBoxHead.appendChild(childCmdDel);
+    childBoxHead.className = "header";
+    childBox.appendChild(childBoxHead);
+    childBox.className = "box";
+    this.dom.appendChild(childBox);
+    console.log(this.dom.innerHTML);
+    var newChild = new TreeBox({parent: this, childs: [], data: data, dom: childBox});
+    this.childs.push(newChild);
+    newChild.render(folded, cmdHide);
+    this.render(unfold, cmdHide);
+    return newChild;
+  },
+  traverse: function() {
+    var boxQueue = [];
+    var stackQueue = [];
+    stackQueue.push(this);
+    while(stackQueue.length > 0) {
+      var root = stackQueue.shift();
+      boxQueue.push(root);
+      console.log("traverse: " + root.data);
+      for (var i = 0; i < root.childs.length; i++) {
+        stackQueue.push(root.childs[i]);
+      } //for
+    } //while
+    return boxQueue;
+  },
+  isFold: function() {
+    if(this.dom.getElementsByClassName("header")[0].getElementsByClassName("fold-sym")[0].innerHTML == "+") {
+      return folded;
+    } else {
+      return unfold;
+    }
+  },
+  toggleFold: function() {
+    if(this.isFold() == folded) {
+      this.render(unfold, cmdHide);
+    } else {
+      this.render(folded, cmdHide);
+    }
+  }
+}
+
+function initTree() {
+  containerBox = new TreeBox({parent: null, childs: [], data: "container", dom: containerDiv});
+  rootBox = containerBox.addChild("IC");
+  rootBox.addChild("Analog");
+  rootBox.childs[0].addChild("CircuitDesign");
+  rootBox.childs[0].addChild("Layout");
+  rootBox.addChild("Digital");
+  rootBox.childs[1].addChild("FrontEnd");
+  rootBox.childs[1].addChild("BackEnd");
+  rootBox.childs[0].traverse();
+}
+
 
 var selectedBox = null;
 
-var boxQueue = new Array();
-var matchQueue = new Array();
-var colorQueue = new Array();
-var stackQueue = new Array();
-var busyFlag = 0;
-var foundFlag = 0;
-var str_pat = "";
 var colorFlash = "#aaa";
 var colorNormal = "#fff";
 var colorClick = "#7FFFD4";
 
-function depthFirst(box, pat) {
-  if(box) {
-    boxQueue.push(box);
-    var tmp = box.childNodes[0].textContent.trim();
-    console.log(tmp);
-    matchQueue.push(tmp == pat);
-    if(tmp == pat) {
-      foundFlag = 1;
-      colorQueue.push(box);
-    }
-    var child = box.firstElementChild;
-    depthFirst(child, pat);
-    for (var i = 1; i < box.childElementCount; i++) {
-      depthFirst(child.nextElementSibling, pat);
-      child = child.nextElementSibling;
-    }
-  }
-}
-
-function breadthFirst(box, pat) {
-  if(box) {
-    stackQueue.push(box);
-  }
-  while(stackQueue.length > 0) {
-    var root = stackQueue.shift();
-    boxQueue.push(root);
-    var tmp = root.childNodes[0].textContent.trim();
-    matchQueue.push(tmp == pat);
-    if(tmp == pat) {
-      foundFlag = 1;
-      colorQueue.push(root);
-    }
-    var child = root.firstElementChild;
-    if(child) {
-      stackQueue.push(child);
-    }
-    for (var i = 1; i < root.childElementCount; i++) {
-      child = child.nextElementSibling;
-      stackQueue.push(child);
-    }
-  }
-}
-function clearRender() {
-  //clear last query results firstly
-  for (var j = 0; j < colorQueue.length; j++) {
-    colorQueue[j].style.background = colorNormal;
-  }
-  colorQueue = [];
-}
-
-function renderBox(time_i, disColor) {
-  var i = 0;
-  boxQueue[i].style.background = colorFlash;
-  var t = setInterval(function() {
-    if(i < (boxQueue.length - 1)) {
-      if(!matchQueue[i] || disColor) {
-        boxQueue[i].style.background = colorNormal;
-      }
-      boxQueue[i+1].style.background = colorFlash;
-    } else if(i == (boxQueue.length -1)) {
-      if(!matchQueue[i] || disColor) {
-        boxQueue[i].style.background = colorNormal;
-      }
-      clearInterval(t);
-      busyFlag = 0;
-      if(!foundFlag && (str_pat != "") && (!disColor)) {
-        alert("Pattern: [" + str_pat + "] Not found !");
-      }
-      foundFlag = 0;
-      newBoxName.focus();
-      newBoxName.select();
-    }
-    i++;
-  }, time_i);
-}
-
-function addBox(box, text) {
-  var newBox = document.createElement('div');
-  newBox.innerHTML = text;
-  if(box == containerDiv) {
-    //root
-    if(containerDiv.childElementCount == 0) {
-      //no root
-      newBox.className = "root";
-    } else {
-      alert("根节点只能有一个！");
-    }
-  } else {
-    //child
-    newBox.className = "child";
-  }
-  box.appendChild(newBox);
-}
-
-function initTree() {
-}
-
 function initButtonEvent() {
-  delBoxBtn.onclick = function() {
-    for (var j = 0; j < colorQueue.length; j++) {
-      var tmp = colorQueue[j].parentNode;
-      tmp.removeChild(colorQueue[j]);
-      if(colorQueue[j] != rootBox) {
-        breadthFirst(rootBox, ""); //rebuild tree
-      } else {
-        //the whole tree del
-        boxQueue = [];
-      }
-    }
-    newBoxName.focus();
-    newBoxName.select();
-  }
-  addBoxBtn.onclick = function() {
-    var boxName = newBoxName.value.trim();
-    if(boxName != "") {
-      if(selectedBox != null) {
-        var newBox = document.createElement('div');
-        newBox.innerHTML = boxName;
-        if(selectedBox == containerDiv) {
-          //root
-          if(selectedBox.childElementCount >= 1) {
-            alert("根节点只能有一个！");
-          } else {
-            newBox.className = "root";
-            //newBox.style.height = 300;
-            selectedBox.appendChild(newBox);
-          }
-        } else {
-          //child elements
-          newBox.className = "child";
-          //newBox.style.height = selectedBox.clientHeight - 40;
-          selectedBox.appendChild(newBox);
-        }
-      } else {
-        alert("添加新节点之前请选择其父节点！");
-      }
-    } else {
-      alert("新节点名字不能为空！");
-    }
-    newBoxName.focus();
-    newBoxName.select();
-  }
-  //only one body in this page
   containerDiv.onclick = function(event) {
     console.log("click on: " + event.target.nodeName + " " + event.target.childNodes[0].textContent);
-    if(event.target.nodeName == "DIV") {
-      clearRender(); //remove last select box
-      selectedBox = event.target;
-      if(event.target.id != "container") {
-        selectedBox.style.background = colorClick;
-        colorQueue.push(selectedBox);
+    var domBox = event.target;
+    if(domBox.nodeName == "DIV" && domBox != containerDiv) {
+      while(domBox.className != "box") {
+        domBox = domBox.parentNode;
       }
-    }
-    newBoxName.focus();
-    newBoxName.select();
-  }
+      selectedBox = domBox.TreeBox;
+      console.log("click on box: " + selectedBox.data);
+      switch (event.target.className) {
+        case "fold-sym":
+          selectedBox.toggleFold();
+          break;
+        case "add":
+          selectedBox.addChild(prompt("请输入新节点名字："));
+          break;
+        case "del":
+          break;
+        default:
+      } //switch
+    } //if
+  } //on click
 }
 
 function init() {
   initButtonEvent();
-  busyFlag = 0;
-  foundFlag = 0;
   initTree();
-  breadthFirst(rootBox, ""); //build tree
 }
 
 init();
