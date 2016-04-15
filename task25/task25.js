6be6170d70bb87989bcd296136fff302fe4f2da5
icsoc <icsoc@qq.com>
2016-04-16 00:49:22 +0800

    add and del are ok

--- task25/task25.js
+++ task25/task25.js
@@ -1,15 +1,15 @@
-var delBoxBtn = document.getElementById("del-box-btn");
-var addBoxBtn = document.getElementById("add-box-btn");
+var queryBtn = document.getElementById("query-btn");
+var queryText = document.getElementById("query-text");
 var newBoxName = document.getElementById("new-box-name");
-var containerDiv = document.getElementById("container");
+var containerDiv = document.getElementsByClassName("container")[0];
 
 var containerBox = null;
 var rootBox = null;
+var selectedBox = null;
+var matchBox = [];
 
 var folded = true;
 var unfold = false;
-var cmdShow = true;
-var cmdHide = false;
 //TreeBox defines the tree node data structure for build/search/render
 function TreeBox(box) {
   this.parent = box.parent;
@@ -21,8 +21,12 @@ function TreeBox(box) {
 
 TreeBox.prototype = {
   constructor: TreeBox,
-  render: function(folded, cmdShow) {
-    if(this.childs.length == 0) {
+  render: function(folded) {
+    if((this.dom == containerDiv) && (this.childs.length == 0)) {
+      this.dom.className = "container tip-root";
+      this.dom.innerHTML = "请单击灰色区域添加根节点！";
+      return;
+    } else if(this.childs.length == 0) {
       this.dom.getElementsByClassName("header")[0].getElementsByClassName("fold-sym")[0].innerHTML = " ";
     } else {
       this.dom.getElementsByClassName("header")[0].getElementsByClassName("fold-sym")[0].innerHTML = folded ? "+" : "-";
@@ -32,16 +36,9 @@ TreeBox.prototype = {
         }
       }
     }
-    if(cmdShow) {
-      this.dom.getElementsByClassName("header")[0].getElementsByClassName("add")[0].className = "add";
-      this.dom.getElementsByClassName("header")[0].getElementsByClassName("del")[0].className = "del";
-    } else {
-      this.dom.getElementsByClassName("header")[0].getElementsByClassName("add")[0].className = "add hide";
-      this.dom.getElementsByClassName("header")[0].getElementsByClassName("del")[0].className = "del hide";
-    }
   },
   addChild: function(data) {
-    if(data.trim() == "") {
+    if((data || "").trim() == "") {
       alert("节点内容为空！");
       return this;
     }
@@ -70,17 +67,34 @@ TreeBox.prototype = {
     console.log(this.dom.innerHTML);
     var newChild = new TreeBox({parent: this, childs: [], data: data, dom: childBox});
     this.childs.push(newChild);
-    newChild.render(folded, cmdHide);
-    this.render(unfold, cmdHide);
+    newChild.render(folded);
+    this.render(unfold);
     return newChild;
   },
-  traverse: function() {
+  delChild: function() {
+    if(this.childs.length != 0) {
+      for (var i = 0; i < this.childs.length; i++) {
+        this.childs[i].delChild();
+      }
+    }
+    this.parent.dom.removeChild(this.dom);
+    for (var j = 0; j < this.parent.childs.length; j++) {
+      if(this.parent.childs[j] == this) {
+        this.parent.childs.splice(j,1);
+        break;
+      }
+    }
+    this.parent.render(unfold);
+  },
+  traverse: function(value) {
     var boxQueue = [];
     var stackQueue = [];
     stackQueue.push(this);
     while(stackQueue.length > 0) {
       var root = stackQueue.shift();
-      boxQueue.push(root);
+      if(root.data == value) {
+        boxQueue.push(root);
+      }
       console.log("traverse: " + root.data);
       for (var i = 0; i < root.childs.length; i++) {
         stackQueue.push(root.childs[i]);
@@ -97,9 +111,9 @@ TreeBox.prototype = {
   },
   toggleFold: function() {
     if(this.isFold() == folded) {
-      this.render(unfold, cmdHide);
+      this.render(unfold);
     } else {
-      this.render(folded, cmdHide);
+      this.render(folded);
     }
   }
 }
@@ -113,19 +127,21 @@ function initTree() {
   rootBox.addChild("Digital");
   rootBox.childs[1].addChild("FrontEnd");
   rootBox.childs[1].addChild("BackEnd");
-  rootBox.childs[0].traverse();
+  rootBox.traverse("");
 }
-
-
-var selectedBox = null;
-
-var colorFlash = "#aaa";
-var colorNormal = "#fff";
-var colorClick = "#7FFFD4";
 
 function initButtonEvent() {
   containerDiv.onclick = function(event) {
-    console.log("click on: " + event.target.nodeName + " " + event.target.childNodes[0].textContent);
+    if((event.target == containerDiv) && (event.target.TreeBox.childs.length == 0)) {
+      var str = prompt("请输入根节点名字：");
+      if((str != null) && (str.trim() != "")) {
+        containerDiv.innerHTML = "";
+        containerDiv.className = "container";
+        rootBox = containerBox.addChild(str.trim());
+      }
+      return;
+    }
+    console.log("click on: " + event.target.nodeName + " " + event.target.childNodes[0].textContent || "");
     var domBox = event.target;
     if(domBox.nodeName == "DIV" && domBox != containerDiv) {
       while(domBox.className != "box") {
@@ -134,17 +150,45 @@ function initButtonEvent() {
       selectedBox = domBox.TreeBox;
       console.log("click on box: " + selectedBox.data);
       switch (event.target.className) {
+        case "header":
+        case "data":
         case "fold-sym":
           selectedBox.toggleFold();
           break;
         case "add":
-          selectedBox.addChild(prompt("请输入新节点名字："));
+          selectedBox.addChild(prompt("请输入新节点名字："));//cancel returns null
           break;
         case "del":
+          selectedBox.delChild();
           break;
         default:
       } //switch
     } //if
+  } //on click
+  queryBtn.onclick = function() {
+    //clear hilight
+    if(matchBox) {
+      for (var i = 0; i < matchBox.length; i++) {
+        matchBox[i].dom.className = "box";
+      }
+    }
+    var value = queryText.value.trim();
+    if(value == "") {
+      alert("查询内容为空！");
+      return;
+    } else {
+      if(containerDiv.TreeBox.childs.length != 0) {
+        matchBox = rootBox.traverse(value);
+      } //if
+      if(matchBox.length == 0) {
+        alert("没找到：" + value);
+        return;
+      } //if
+      for (var j = 0; j < matchBox.length; j++) {
+        console.log(matchBox[i].data);
+        matchBox[i].dom.className = "box hilight";
+      }
+    } //if else
   } //on click
 }
 
